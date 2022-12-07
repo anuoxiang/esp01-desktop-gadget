c98b5fe48c2446ebf23857e683b4b9b0527853c3
强哥 <django@leadleo.com>
2022-12-07 13:35:16 +0800

    .

---
+++ ESP01-Multitask-Display/calender.cpp
@@ -0,0 +1,11 @@
+#include "calender.h"
+
+// 主过程
+ProcessInfo calender_main(void)
+{
+
+  ProcessInfo result;
+  result.period = 1000;
+  result.status = RUNNING;
+  return result;
+}
--- ESP01-Multitask-Display/calender.h
+++ ESP01-Multitask-Display/calender.h
@@ -1,4 +1,11 @@
 #ifndef _CALENDER_H_
 #define _CALENDER_H_
 
+#include "util.h"
+
+// 仅用来测试，没有实质内容
+
+// 主过程
+ProcessInfo calender_main(void);
+
 #endif
